45daf2760262c05f36f7e5306f524779118fbfdb
Mohd Asad~ <mohdasad024@gmail.com>
2025-11-05 22:51:41 +0530

    final commit

--- image-search-project/client/App.jsx
+++ image-search-project/client/App.jsx
@@ -3,11 +3,10 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./component/Login";
-import { AuthProvider} from "./contexts/authContext";
+import { AuthProvider } from "./contexts/authContext";
 import ProtectedRoute from "./component/ProtectedRoute";
 import NotFound from "./pages/NotFound";
 import Header from "./component/Header";
-
 
 function App() {
   return (
@@ -19,9 +18,10 @@ function App() {
             <Route
               path="/"
               element={
-               
+                <ProtectedRoute>
+                  {" "}
                   <Home />
-                
+                </ProtectedRoute>
               }
             />
             <Route path="/login" element={<Login />} />
